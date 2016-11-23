7c8d65b5e78bed8cec146e192e2db44bd5bc5c3d
MALDRU <ing.andres.latorre@gmail.com>
2016-11-22 21:54:47 -0500

    Inicio de tutorial Login

---
+++ Login/ui/login.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Control de acceso</title>
+</head>
+<body>    
+    <form action="">
+        
+    </form>
+</body>
+</html>
